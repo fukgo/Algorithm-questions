6827f8d1c23fd17d71e97f2907cdfad1f255b34e
luyin din <dly2356790469@gmail.com>
2024-03-17 18:12:27 +0800

    Delete Algorithm-questions directory

--- Algorithm-questions/智能体系列赛/1.py
+++
@@ -1,6 +0,0 @@
-def manhattan_distance(p1,p2):
-    return abs(p1[0]-p2[0])+abs(p1[1]+p2[1])
-
-original_point = list(map(int,input().split()))
-n = int(input())
-points = [list(map(int,input().split())) for _ in range(n)]
